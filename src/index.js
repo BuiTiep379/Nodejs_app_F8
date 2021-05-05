4911bc56c9ecb485a6c7de339f5a4eaf1f4491b3
BuiTiep379 <buitiep379@gmail.com>
2021-05-05 13:13:20 +0700

    install prettier husky lint-staged

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-            express.urlencoded({
-            extended: true,
+                express.urlencoded({
+                extended: true,
     }),
 );
 app.use(express.json());
