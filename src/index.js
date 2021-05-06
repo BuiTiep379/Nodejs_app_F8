f4223580b6b315734101612a8a42f113bd858899
BuiTiep379 <buitiep379@gmail.com>
2021-05-06 12:12:35 +0700

    admin page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ db.connect();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
     express.urlencoded({
         extended: true,
@@ -18,6 +19,10 @@ app.use(express.json());
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
+app.set('css', path.join(__dirname, '/public/css'));
+app.set('js', path.join(__dirname, '/public/js'));
+app.set('images', path.join(__dirname, '/public/images'));
+app.set('vendors', path.join(__dirname, '/public/vendors'));
 console.log(path.join(__dirname, 'views'));
 
 app.use(morgan('combined'));
